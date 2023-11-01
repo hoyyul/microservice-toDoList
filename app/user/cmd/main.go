dfafe282662b79dbdf62c840299bd66c4ef18353
halu <hlu07472@gmail.com>
2023-11-01 23:51:17 +0900

    debug for user server and change etcd registrar

--- app/user/cmd/main.go
+++ app/user/cmd/main.go
@@ -17,26 +17,25 @@ func main() {
 	setting.InitConfig()
 	setting.InitLogger()
 
-	// local initial setting
+	// local server initial setting
 	dao.InitDB()
-
 	userName := global.Config.Services["user"].Name
 	userAddr := global.Config.Services["user"].Address
-
-	// register service to etcd
-	etcdRegistrar := eTcd.NewRegistrar(global.Config.Etcd.Address) // etcd
-	defer etcdRegistrar.UnRegister()
-
+	etcdAddrs := []string{global.Config.Etcd.Address}
+	etcdRegistrar := eTcd.NewRegistrar(etcdAddrs)
+	defer etcdRegistrar.Stop()
 	server := eTcd.Server{
 		Name: userName,
 		Addr: userAddr,
 	}
-	if err := etcdRegistrar.Register(server, 10); err != nil { // my server
+
+	// register service to etcd
+	if err := etcdRegistrar.Register(server, 10); err != nil {
 		global.Logger.Fatalln("Failed to register service to Etcd Server")
 		return
 	}
 
-	// bind grpc service
+	// listen to server
 	listener, err := net.Listen("tcp", userAddr)
 	if err != nil {
 		global.Logger.Panic(err)
@@ -44,11 +43,13 @@ func main() {
 	}
 	global.Logger.Printf("Listeing to %v", listener.Addr())
 
-	grpcSrv := grpc.NewServer()
-	pb.RegisterUserServiceServer(grpcSrv, service.NewUserService())
+	// register my server to grpc server
+	grpcServer := grpc.NewServer()
+	defer grpcServer.Stop()
+	pb.RegisterUserServiceServer(grpcServer, service.NewUserService())
 
-	// start grpc server
-	if err := grpcSrv.Serve(listener); err != nil {
+	// enable grpc  to serve
+	if err := grpcServer.Serve(listener); err != nil {
 		return
 	}
 	global.Logger.Printf("User server is running on %s", userAddr)
--- pkg/eTcd/registrar.go
+++ pkg/eTcd/registrar.go
@@ -2,69 +2,136 @@ package eTcd
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
+	"micro-toDoList/global"
+	"time"
 
 	clientv3 "go.etcd.io/etcd/client/v3"
-	"go.etcd.io/etcd/client/v3/naming/endpoints"
 )
 
 type Registrar struct {
-	EtcdAddr string
+	EtcdAddrs   []string
+	DialTimeout int
 
-	// hiden info
-	cli *clientv3.Client
-	srv Server
+	Cli    *clientv3.Client
+	Srv    Server
+	SrvTTL int64
+
+	keepAliveCh <-chan *clientv3.LeaseKeepAliveResponse
+	closeCh     chan struct{}
+	LeaseID     clientv3.LeaseID
 }
 
-func NewRegistrar(etcdAddr string) *Registrar {
-	return &Registrar{EtcdAddr: etcdAddr}
+func NewRegistrar(etcdAddrs []string) *Registrar {
+	return &Registrar{
+		EtcdAddrs:   etcdAddrs,
+		DialTimeout: 5,
+		closeCh:     make(chan struct{}),
+	}
 }
 
 func (r *Registrar) Register(srv Server, ttl int64) error {
 	// set server
-	r.srv = srv
+	r.Srv = srv
 
-	// create etcd conn
-	cli, err := clientv3.NewFromURL(fmt.Sprintf("http://%s", r.EtcdAddr))
+	// create an etcd client to connect etcd server
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   r.EtcdAddrs,
+		DialTimeout: 10 * time.Second,
+	})
 	if err != nil {
+		global.Logger.Panicln("Failed to connect etcd server")
 		return err
 	}
-	r.cli = cli
+	r.Cli = cli
 
-	// grant lease
-	lease, err := r.cli.Grant(context.TODO(), ttl)
-	if err != nil {
-		return err
-	}
-
-	// add service to endpoint
-	em, err := endpoints.NewManager(r.cli, BuildPrefix(r.srv))
-	if err != nil {
-		return err
-	}
-	em.AddEndpoint(context.TODO(), BuildRegisterPath(r.srv), endpoints.Endpoint{Addr: r.srv.Addr}, clientv3.WithLease(lease.ID))
-
-	// revoke
-	ch, _ := r.cli.KeepAlive(context.TODO(), lease.ID)
-
-	// consume the ka in channel to avoid max capacity
-	go func() {
-		for {
-			ka := <-ch
-			if ka != nil {
-				continue
-			}
-		}
-	}()
+	go r.keepAlive()
 
 	return nil
 }
 
-func (r *Registrar) UnRegister() error {
-	em, err := endpoints.NewManager(r.cli, r.srv.Name)
+func (r *Registrar) register() error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(r.DialTimeout)*time.Second)
+	defer cancel()
+
+	// 1. Grant; create lease
+	lease, err := r.Cli.Grant(ctx, r.SrvTTL)
 	if err != nil {
 		return err
 	}
 
-	return em.DeleteEndpoint(context.TODO(), fmt.Sprintf("%s:%s", r.srv.Name, r.srv.Addr))
+	// 2. KeepAlive; update the lease for a time period
+	r.LeaseID = lease.ID
+	r.keepAliveCh, err = r.Cli.KeepAlive(ctx, r.LeaseID)
+	if err != nil {
+		return err
+	}
+
+	// 3. Put; save key-value in etcd, etcd is a key-value store
+	key := BuildRegisterPath(r.Srv)     // key
+	srvInfo, err := json.Marshal(r.Srv) // value
+	if err != nil {
+		return err
+	}
+
+	_, err = r.Cli.Put(context.Background(), key, string(srvInfo), clientv3.WithLease(r.LeaseID))
+	if err != nil {
+		return err
+	}
+
+	return err
+}
+
+func (r *Registrar) keepAlive() {
+	ticker := time.NewTicker(time.Duration(r.DialTimeout) * time.Second)
+	for {
+		select { // channel 版本的switch；看看那个准备好了； 如果多个准备好了就随机选一个
+		case <-r.closeCh:
+			if err := r.unRegister(); err != nil {
+				global.Logger.Error(err)
+			}
+			// release the capacity of keepAlive channel
+		case resp := <-r.keepAliveCh:
+			if resp == nil {
+				if err := r.register(); err != nil {
+					global.Logger.Error(err)
+				}
+			}
+		case <-ticker.C:
+			resp, err := r.Cli.Get(context.Background(), BuildRegisterPath(r.Srv))
+			if err != nil {
+				global.Logger.Panicln(err)
+				if err := r.register(); err != nil {
+					global.Logger.Error(err)
+				}
+			}
+			if resp.Count == 0 {
+				if err := r.register(); err != nil {
+					global.Logger.Error(err)
+				}
+			}
+		}
+
+	}
+
+}
+
+func (r *Registrar) Stop() {
+	r.closeCh <- struct{}{}
+}
+
+func (r *Registrar) unRegister() error {
+	// delete registered service
+	_, err := r.Cli.Delete(context.Background(), BuildRegisterPath(r.Srv))
+	if err != nil {
+		return err
+	}
+
+	// revoke lease
+	_, err = r.Cli.Revoke(context.Background(), r.LeaseID)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
